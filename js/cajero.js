441bb5ecbd6340cbc9afda64b40eb98a75a45670
HenrryJK <henrryorbezo@upeu.edu.pe>
2020-05-22 03:37:55 -0500

    Cajero-HenrryOrbezo

--- js/cajero.js
+++ js/cajero.js
@@ -1,24 +1,100 @@
-var num = document.getElementById("datos");
-function add(n) {
-    var x = num.value;
-    num.value = x + n;
-}
-function cancel() {
-    num.value="";
-    document.getElementById("mensaje").innerHTML=""
-}
-function cambiar() {
-    document.getElementById("m1").innerHTML = "<h1>MENU PRINCIAL</h1>";
-}
-function login(){
-    var pass = document.getElementById("datos").value;
-    if (pass == "080120") {
-        document.getElementById("m1").style.display="nome";
-        document.getElementById("cambiar();").style.display="block";
-       
-        
+class Usuario{
+    constructor(password){
+        this.password= password;
+    };
+    verificacion(){
+        if (this.password=="080120") {
+            return("La sesion se ha iniciado..")
+        }else{
+          return("La contraseña es incorrecta...")     
+        }
+    };
+};
+function logueo() {
+    var usuario= new Usuario(document.getElementById("datos").value);
+    var seguro=usuario.verificacion();
+    if (seguro=="La sesion se ha iniciado..") {
+        document.getElementById("pantalla1").style.display="none";
+        document.getElementById("pantalla2").style.display="block";
     }else{
-    
-        document.getElementById("mensaje").innerHTML="La contraseña es Incorrecta!!!!"
+        document.getElementById("mensaje").value=seguro;
+       
     }
+}
+document.getElementById("num1").addEventListener("click", num1);
+function num1(){
+    let sumado=document.getElementById("num1").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num2").addEventListener("click", num2);
+function num2(){
+    let sumado=document.getElementById("num2").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num3").addEventListener("click", num3);
+function num3(){
+    let sumado=document.getElementById("num3").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num4").addEventListener("click", num4);
+function num4(){
+    let sumado=document.getElementById("num4").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num5").addEventListener("click", num5);
+function num5(){
+    let sumado=document.getElementById("num5").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num6").addEventListener("click", num6);
+function num6(){
+    let sumado=document.getElementById("num6").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num7").addEventListener("click", num7);
+function num7(){
+    let sumado=document.getElementById("num7").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num8").addEventListener("click", num8);
+function num8(){
+    let sumado=document.getElementById("num8").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num9").addEventListener("click", num9);
+function num9(){
+    let sumado=document.getElementById("num9").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("num0").addEventListener("click", num0);
+function num0(){
+    let sumado=document.getElementById("num0").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+}
+document.getElementById("nump").addEventListener("click", nump);
+function nump(){
+    let sumado=document.getElementById("nump").innerHTML;
+    var elemento=document.getElementById("datos");
+    var value=elemento.value;
+    elemento.value=value + sumado;
 }
